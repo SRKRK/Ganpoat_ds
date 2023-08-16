3a7e669c4b1979b079117a25d3c0b89ead1c7f53
SRKRK <radha.diff@gmail.com>
2023-08-16 16:40:36 +0530

    Updated app.py with changes

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
-import pickle
-from flask import Flask, request, app, render_template
-from flask.json import jsonify
+import sys
+import json
 import numpy as np
-import pandas as pd
-app=Flask(__name__)
-##Load the model
-regmodel= pickle.load(open('regmodel.pkl', 'rb'))
-scalar=pickle.load(open('scaling.pkl','rb'))
+import pickle as pkl
+from flask import Flask, request, jsonify, render_template
+app = Flask(_name_)
+# Load the model
+dtr = pkl.load(open('regmodel.pkl', 'rb'))
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api', methods=['post'])
+@app.route('/predict_api', methods=['POST'])
 def predict_api():
-    data=request.json['data']
-    print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
-    output=regmodel.predict(new_data)
-    print(output[0])
-    return jsonify(output[0])
+    data = request.json['data']
+    input_data = np.array(list(data.values())).reshape(1, -1)
+    output = dtr.predict(input_data)
+    return jsonify(output.tolist())  # Convert numpy array to a Python list
+
+@app.route('/predict',methods =['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input = np.array(data).reshape(1, -1)
+    print(final_input)
+    output = dtr.predict(final_input)[0]
+    return render_template("home.html",prediction_text = "The internal feasibility value is {}".format(output))
 
 
-if __name__=="__main__":
+if _name_ == "_main_":
     app.run(debug=True)
